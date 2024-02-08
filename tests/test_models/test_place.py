118884b441ea004c07c35ec4196bf7bd7b3eba2c
Brendon45 <jejebrendon722@gmail.com>
2024-02-08 12:59:24 -0800

    Test models

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -50,6 +50,19 @@ class TestPlace(unittest.TestCase):
         """Verify the methods of the place class"""
         with self.subTest(msg='Inheritance'):
             self.assertTrue(issubclass(Place, BaseModel))
-    
-    
 
+        with self.subTest(msg='Attributes'):
+            self.assertIsInstance(Place.city_id, str)
+            self.assertIsInstance(Place.user_id, str)
+            self.assertIsInstance(Place.name, str)
+            self.assertIsInstance(Place.description, str)
+            self.assertIsInstance(Place.number_rooms, int)
+            self.assertIsInstance(Place.number_bathrooms, int)
+            self.assertIsInstance(Place.max_guest, int)
+            self.assertIsInstance(Place.price_by_night, int)
+            self.assertIsInstance(Place.latitude, float)
+            self.assertIsInstance(Place.longitude, float)
+            self.assertIsInstance(Place.amenity_ids, list)
+
+if __name__ == '__main__':
+    unittest.main()
