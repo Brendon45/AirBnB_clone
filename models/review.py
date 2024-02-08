efb595ab2c9638d5950e63b0e8b0efa3f6a33bb6
Brendon45 <jejebrendon722@gmail.com>
2024-02-07 16:01:30 -0800

    Models - The Console

---
+++ models/review.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module for the review class
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    A class to handle customer reviews
+    Public class attributes:
+        - place_id: string - empty string: it will be the Place.id
+        - user_id: string - empty string: it will be the User.id
+        - text: string - empty string
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a Review instance."""
+        super().__init__(*args, **kwargs)
